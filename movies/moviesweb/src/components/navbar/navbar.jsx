efc5612442fcd0296fbd57bd7675b51c26144bec
jayson7 <lexxiijoo70@gmail.com>
2021-12-29 02:55:43 +0100

    movies

---
+++ movies/moviesweb/src/components/navbar/navbar.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+              <li>
+                <a href="/"> Home </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
