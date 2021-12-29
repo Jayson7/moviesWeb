2f19b97933c8fb8b3e4a62d08ed0ba56a7c72c2a
jayson7 <lexxiijoo70@gmail.com>
2021-12-29 04:02:52 +0100

    movies

--- movies/moviesweb/src/components/hompage/index.jsx
+++ movies/moviesweb/src/components/hompage/index.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import "./index.css";
 
-function Homepage() {
-  const api_key = 
-  return (
-    <div>
-      
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import "./index.css";
+class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactlist: [],
+      // detailslist: [],
+      // items: [],
+      // iid: {},
+    };
+  }
+
+  componentDidMount() {
+    this.refreshList();
+
+    // clearInterval(this.refreshList());
+  }
+  refreshList = () => {
+  const api_key = "api_key=7768d9b2e7b144c9996e688064e2b36d";
+  const base_url = "https://api.themoviedb.org/3";
+  const page = "page=4";
+  const top_rated =
+    base_url + "/movie/top_rated?" + api_key + "&language=en-US&" + page;
+
+  fetch(top_rated)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+    })    
+  }
 
 export default Homepage;
