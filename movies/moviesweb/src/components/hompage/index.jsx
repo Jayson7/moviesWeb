35dccf0def2259754e9ac7a690a4674a4ed58960
jayson7 <lexxiijoo70@gmail.com>
2021-12-29 04:06:41 +0100

    movies

--- movies/moviesweb/src/components/hompage/index.jsx
+++ movies/moviesweb/src/components/hompage/index.jsx
@@ -1,12 +1,11 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./index.css";
 class Homepage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contactlist: [],
-      // detailslist: [],
+      topRatedList: [],
+      popularList: [],
       // items: [],
       // iid: {},
     };
@@ -18,18 +17,25 @@ class Homepage extends Component {
     // clearInterval(this.refreshList());
   }
   refreshList = () => {
-  const api_key = "api_key=7768d9b2e7b144c9996e688064e2b36d";
-  const base_url = "https://api.themoviedb.org/3";
-  const page = "page=4";
-  const top_rated =
-    base_url + "/movie/top_rated?" + api_key + "&language=en-US&" + page;
+    const api_key = "api_key=7768d9b2e7b144c9996e688064e2b36d";
+    const base_url = "https://api.themoviedb.org/3";
+    const page = "page=4";
+    const top_rated =
+      base_url + "/movie/top_rated?" + api_key + "&language=en-US&" + page;
 
-  fetch(top_rated)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-
-    })    
+    fetch(top_rated)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  render() {
+    return (
+      <div>
+        <h1>homepage</h1>
+      </div>
+    );
   }
+}
 
 export default Homepage;
