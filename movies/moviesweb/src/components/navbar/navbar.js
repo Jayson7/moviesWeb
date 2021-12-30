0d769e1b7a31feaa1f89a879e094c9e3fce16d7b
jayson7 <lexxiijoo70@gmail.com>
2021-12-30 14:44:58 +0100

    movies

--- movies/moviesweb/src/components/navbar/navbar.js
+++ movies/moviesweb/src/components/navbar/navbar.js
@@ -1,11 +1,13 @@
-const navactive = document.getElementsByClassName("active");
-const navclass = document.getElementsByClassName("nav");
-
+// const navactive = document.getElementsByClassName("active");
+// const navclass = document.getElementsByClassName("nav");
+const nav = document.querySelectorAll("nav")[0];
 window.addEventListener("resize", sizerTriggers);
 
 function sizerTriggers() {
     if (window.innerWidth <= 814) {
         console.log("yes");
+        console.log(nav);
+        // console.log(nav.classList());
     }
 }
 sizerTriggers();
