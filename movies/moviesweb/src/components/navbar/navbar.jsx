27ad377c962e3b789d215f46d0e7e1fba529a099
jayson7 <lexxiijoo70@gmail.com>
2021-12-30 10:05:53 +0100

    movies

--- movies/moviesweb/src/components/navbar/navbar.jsx
+++ movies/moviesweb/src/components/navbar/navbar.jsx
@@ -10,7 +10,7 @@ class Navbar extends Component {
           </h6>
         </div>
         <header>
-          <nav>
+          <nav className="nav">
             <ul>
               <li>
                 <a href="/"> Home </a>
