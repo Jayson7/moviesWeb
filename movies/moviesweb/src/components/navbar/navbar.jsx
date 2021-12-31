7f2970f8a240ca943e0d81a2dec46eb685f5a493
jayson7 <lexxiijoo70@gmail.com>
2021-12-31 15:04:52 +0100

    movies

--- movies/moviesweb/src/components/navbar/navbar.jsx
+++ movies/moviesweb/src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./navbar.css";
 
 class Navbars extends Component {
@@ -12,66 +12,68 @@ class Navbars extends Component {
           </h6>
         </div>
         <header>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button
-              className="navbar-toggler d-lg-none"
-              type="button"
-              data-toggle="collapse"
-              data-target="#collapsibleNavId"
-              aria-controls="collapsibleNavId"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="collapsibleNavId">
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item ">
-                  <a className="nav-link" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Link
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="/"
-                    id="dropdownId"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
+          <Router>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <button
+                className="navbar-toggler d-lg-none"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="collapsibleNavId">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li className="nav-item ">
+                    <a className="nav-link" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Link
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="/"
+                      id="dropdownId"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="dropdownId">
+                      <a className="dropdown-item" href="/">
+                        Action 1
+                      </a>
+                      <a className="dropdown-item" href="/">
+                        Action 2
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                  <input
+                    className="form-control mr-sm-2"
+                    type="text"
+                    placeholder="Search"
+                  />
+                  <button
+                    className="btn btn-outline-success my-2 my-sm-0"
+                    type="submit"
                   >
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="dropdownId">
-                    <a className="dropdown-item" href="/">
-                      Action 1
-                    </a>
-                    <a className="dropdown-item" href="/">
-                      Action 2
-                    </a>
-                  </div>
-                </li>
-              </ul>
-              <form className="form-inline my-2 my-lg-0">
-                <input
-                  className="form-control mr-sm-2"
-                  type="text"
-                  placeholder="Search"
-                />
-                <button
-                  className="btn btn-outline-success my-2 my-sm-0"
-                  type="submit"
-                >
-                  Search
-                </button>
-              </form>
-            </div>
-          </nav>
+                    Search
+                  </button>
+                </form>
+              </div>
+            </nav>
+          </Router>
         </header>
       </div>
     );
